89703e2fbf875c5a9831d254dcabae1f36d89fd8
Chris Tyson <christyson@users.noreply.github.com>
2020-05-29 16:57:53 -0400

    Update jenkins-create-da-scan.py

--- jenkins-create-da-scan.py
+++ jenkins-create-da-scan.py
@@ -16,7 +16,7 @@ from urllib.parse import urlparse
 api_id = os.getenv("VeraID")
 api_secret = os.getenv("VeraPW")
 dynamic_target = os.getenv("Dyanamic_Target")
-print("Dynamic Target is: " + dynamic_target
+print("Dynamic Target is: " + dynamic_target)
 dynamic_job = os.getenv("JOB_NAME") #Dynamic Job name will be same as Jenkins project name
 
 
